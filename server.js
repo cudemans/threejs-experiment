3407fec38ee389bd8e14bcb598eb0ece1666153d
Chris Udemans <chris@udemans.xyz>
2021-07-07 22:26:12 +0200

    init

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const path = require("path")
+
+const app = express()
+app.use(express.static(path.join(__dirname, "/public")))
+app.use("/build/", express.static(path.join(__dirname, "node_modules/three/build")))
+app.use("/jsm/", express.static(path.join(__dirname, "node_modules/three/examples/jsm")))
+
+app.listen(4000, () => {
+    console.log("Server running")
+})
